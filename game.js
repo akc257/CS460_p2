b3a4d86eaeedf33e43ee612660afaf0ac8e1dfc5
Austin <akc257@nau.edu>
2019-10-06 17:40:06 -0700

    Update game.js

--- game.js
+++ game.js
@@ -19,14 +19,32 @@ function ready() {
     frames.push(PIXI.Texture.fromFrame('alien' + i + '.png'));
   }
 
-  runner = new PIXI.extras.MovieClip(frames);
-  runner.scale.x = 4;
-  runner.scale.y = 4;
-  runner.position.x = 200;
-  runner.position.y = 200;
-  runner.animationSpeed = 0.1;
-  runner.play();
-  stage.addChild(runner);
+  alien = new PIXI.extras.MovieClip(frames);
+  alien.scale.x = 2;
+  alien.scale.y = 2;
+  alien.position.x = 360;
+  alien.position.y = 360;
+  alien.animationSpeed = 0.1;
+  alien.play();
+  stage.addChild(alien);
+
+  alien_two = new PIXI.extras.MovieClip(frames);
+  alien_two.scale.x = 2;
+  alien_two.scale.y = 2;
+  alien_two.position.x = 340;
+  alien_two.position.y = 340;
+  alien_two.animationSpeed = 0.09;
+  alien_two.play();
+  stage.addChild(alien_two);
+
+  alien_three = new PIXI.extras.MovieClip(frames);
+  alien_three.scale.x = 1.5;
+  alien_three.scale.y = 1.5;
+  alien_three.position.x = 380;
+  alien_three.position.y = 340;
+  alien_three.animationSpeed = 0.9;
+  alien_three.play();
+  stage.addChild(alien_three);
 }
 
 function animate() {
