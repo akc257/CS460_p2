bde5402dba984574c66ac1cd0669f9631e2ce1ec
Austin <akc257@nau.edu>
2019-10-07 20:38:26 -0700

    tween and audio

--- game.js
+++ game.js
@@ -12,13 +12,18 @@ var alien;
 var alien_two;
 var alien_three;
 var man;
+var sound;
 
 //Creating sprites
 var start_screen_bg = new PIXI.Sprite(PIXI.Texture.fromImage("start_screen.png") );
+var game_over = new PIXI.Sprite(PIXI.Texture.fromImage("game_over.png") );
 var background = new PIXI.Sprite(PIXI.Texture.fromImage("background.png") );
 var ship = new PIXI.Sprite(PIXI.Texture.fromImage("ship.png") );
 var man = new PIXI.Sprite(PIXI.Texture.fromImage("man3.png") );
 var area_51 = new PIXI.Sprite(PIXI.Texture.fromImage("51.png") );
+var thank = new PIXI.Sprite(PIXI.Texture.fromImage("thank.png"));
+var new_x = Math.floor(Math.random() * 100) + 50;
+var new_y = Math.floor(Math.random() * 100) + 50;
 
 //add start screen to scene graph
 var start_screen = new PIXI.Container();
@@ -46,7 +51,7 @@ function mouseStartHandler(e)
   stage.addChild(game_screen);
 }
 
-//call mousehandler when screen clicked on
+//call mousehandler when screen click
 start_screen_bg.interactive = true;
 start_screen_bg.on('mousedown', mouseStartHandler);
 
@@ -54,10 +59,10 @@ PIXI.loader
   .add("assets.json")
   .load(ready);
 
+
 function ready() {
 
   var frames = [];
-  var frames2 = [];
 
   for (var i=1; i<=4; i++) {
     frames.push(PIXI.Texture.fromFrame('alien' + i + '.png'));
@@ -76,32 +81,84 @@ function ready() {
   ship.position.y = -210;
   game_screen.addChild(ship);
 
+  area_51.position.x = 0;
+  area_51.position.y = 160;
+  game_screen.addChild(area_51);
+
   man.position.x = 0;
   man.position.y = 150;
   game_screen.addChild(man);
 }
 
-function keydownEventHandler(e) {
+// code from DR PALMER
+// for sound usage
+PIXI.loader
+  .add("fly.mp3")
+  .load(sound_1);
 
+function sound_1() {
+  sound = PIXI.audioManager.getAudio("fly.mp3");
+}
+
+
+function keydownEventHandler(e) {
   if (e.keyCode == 87) { // W key
     man.position.y -= 10;
-    // alien_two.position.y += 10;
+    sound.play();
+    // sound implemented but I don't hear
   }
 
   if (e.keyCode == 83) { // S key
     man.position.y += 10;
-    // alien_two.position.y -= 10;
   }
 
   if (e.keyCode == 65) { // A key
     man.position.x -= 10;
-    // alien_two.position.x += 10;
   }
 
   if (e.keyCode == 68) { // D key
     man.position.x += 10;
-    // alien_two.position.x -= 10;
   }
+
+  if (man.position.x == alien.position.x && man.position.y == alien.position.y ){
+
+      if (e.keyCode == 87) { // W key
+        man.position.y -= 10;
+        alien.position.y -= 10;
+      }
+
+      if (e.keyCode == 83) { // S key
+        man.position.y += 10;
+        alien.position.y += 10;
+      }
+
+      if (e.keyCode == 65) { // A key
+        man.position.x -= 10;
+        alien.position.x -= 10;
+      }
+
+      if (e.keyCode == 68) { // D key
+        man.position.x += 10;
+        alien.position.x += 10;
+      }
+  }
+}
+
+setInterval(end_game, 500);
+
+function end_game()
+{
+    if(alien.position.x > -20 && alien.position.x < 20 &&
+        alien.position.y > 150 && alien.position.y < 180){
+            stage.removeChild(game_screen);
+            stage.addChild(game_over);
+
+            thank.position.x = 0;
+            thank.position.y = 0;
+            stage.addChild(thank);
+            createjs.Tween.get(hero.position).to({x: new_x, y: new_y}, 1000, createjs.Ease.bounceOut);
+
+        }
 }
 
 document.addEventListener('keydown', keydownEventHandler);
